7b7c6d334c23a85e5e980142fb540e9d7ac72b8c
etome3 <agfsabogus@gmail.com>
2026-05-15 14:39:21 +1000

    add pc branch

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.guifont = "JetBrainsMono Nerd Font:h14"
 opt.relativenumber = false
 
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
 
+opt.shell = "pwsh"
+opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+opt.shellquote = ""
+opt.shellxquote = ""
 vim.g.lazyvim_python_lsp = "ty"
 
 vim.g.neovide_cursor_animation_length = 0
