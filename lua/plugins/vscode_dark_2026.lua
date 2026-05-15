c4014f2ccc8dc10cbcb80fb6501e5fd161025f16
etome3 <agfsabogus@gmail.com>
2026-05-15 10:31:56 +1000

    Remove stale .bak files from plugins

--- lua/plugins/vscode_dark_2026.lua
+++ lua/plugins/vscode_dark_2026.lua
@@ -1,11 +1,10 @@
 return {
-  { "rktjmp/lush.nvim", lazy = true },
   {
     "etome3/vscode_dark_2026.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme vscode_dark_2026")
+      vim.cmd.colorscheme("vscode_dark_2026")
     end,
   },
 }
